8b0655ef3a8ae06be9166698af7f742b04e1a4df
Piotr Ferenc <ferenc.piotr@outlook.com>
2021-09-15 13:16:00 +0200

    Add Products project structure

---
+++ src/Modules/Products/OrderBin.Modules.Products.Api/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using OrderBin.Modules.Products.Core;
+
+[assembly:InternalsVisibleTo("OrderBin.Bootstrapper")]
+namespace OrderBin.Modules.Products.Api
+{
+    public static class Extensions
+    {
+        internal static IServiceCollection AddProducts(this IServiceCollection service)
+        {
+            service.AddCore();
+            
+            return service;
+        }
+    }
+}
---
+++ src/Shared/OrderBin.Infrastructure/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OrderBin.Infrastructure.Api;
+
+[assembly: InternalsVisibleTo("OrderBin.Bootstrapper")]
+
+namespace OrderBin.Infrastructure
+{
+    public static class Extensions
+    {
+        internal static IServiceCollection AddInfrastructure(this IServiceCollection service)
+        {
+            service.AddControllers().ConfigureApplicationPartManager(
+                manager => { manager.FeatureProviders.Add(new InternalControllerFeatureProvider()); }
+            );
+
+            return service;
+        }
+
+        internal static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", context => context.Response.WriteAsync("Works"));
+            });
+
+
+            return app;
+        }
+    }
+}
